b159b0a0c11c5dc2aa8e877c5699ea2f963f5c65
unknown <Administrator@test.xellar.ch>
2020-07-09 21:25:01 +0200

    Add de local

---
+++ locales/de.lua
@@ -0,0 +1,10 @@
+Locales['de'] = {
+  ['novehnearby'] = 'Es ist kein Fahrzeug in der Nähe!',
+  ['cantinside']  = 'You cannot do this from inside a vehicle!',
+  ['arming']      = 'Arming the device',
+  ['vanilla']     = 'The device will explode in %s seconds',
+  ['speed']       = 'The device will explode at the speed of %s %s',
+  ['remote']      = 'Detonate the device by pressing G',
+  ['delayed']     = 'The device will detonate %s seconds after someone enters the vehicle',
+  ['instant']     = 'The device will explode as soon as the driver enters the car'  
+}
